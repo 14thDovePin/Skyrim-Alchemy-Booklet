dd193acf0f89c0c7bf808f0cf1f4f421469dcc04
14thDovePin <therlaf0409@gmail.com>
2022-05-24 06:15:45 +0800

    Added contents to "Ingredients Database" menu page.
    ► Removed existing sample buttons of "Ingredients Database" page.
    ► Added method "update_contents" to "IngredientsDataPanel" of
    "panel_ingredients_data.py" for updating the contents of the page.
    ► Added internal method "_add_ingredient" alongside "update_contents" for
    formatting the color of the button's text.
    ► Added another internal method for the previous internal method. It is the
    function called whenever the button has been released.

--- panel_ingredients_data.py
+++ panel_ingredients_data.py
@@ -1,4 +1,9 @@
+from kivy.clock import Clock
 from kivy.uix.scrollview import ScrollView
+from kv_py_ref import SearchBoxButton
+
+
+from app_api import ParseQuery
 
 
 class IngredientsDataPanel(ScrollView):
@@ -15,7 +20,54 @@ class IngredientsDataPanel(ScrollView):
     """
 
     def __init__(self, **kwargs):
+        """Adds the default and preset custom ingredients to the page."""
         super().__init__(**kwargs)
+        Clock.schedule_once(lambda dt: self.update_contents(), 0.1)
+
+    def update_contents(self):
+        """Updates the content inside of the page."""
+        api = ParseQuery()
+        grid_layout = self.panel_grid
+        color_a, color_b = '#93c47d', '#76a5af'
+        for x, i in enumerate(api.word_pool):
+            if x%2 == 0:
+                self._add_ingredient(grid_layout, color_a, i)
+            else:
+                self._add_ingredient(grid_layout, color_b, i)
+
+    def _add_ingredient(self, base_widget, color, button_text):
+        """Adds a SearchBoxButton rule to a grid layout.
+
+        Arguments
+        ---------
+        base_widget: kivy.uix.gridlayout object
+            The grid layout that is used to add the button wigdet to.
+        color: string
+            The hex color to be used in the button's background.
+        text: string
+            The text that will be used for the button.
+        """
+        text = f'[color={color.upper()}][b][size=20dp]'+button_text[:]
+        base_widget.add_widget(
+            SearchBoxButton(
+                size_hint = [1, None],
+                text = text,
+                padding = [5, 5],
+                background_color = (64/255, 64/255, 64/255, 1),
+                on_release = lambda ins: self._search_ingredient(text)
+                )
+            )
+
+    def _search_ingredient(self, text):
+        """Toggles the page and searches the ingredient.
+
+        Arguments
+        ---------
+        text: string
+            The string that will be passed to search the ingredient.
+        """
+        self.parent.front_widget.search_bar.suggestion_search(text)
+        self.toggle_page()
 
     from toggle_page import toggle_page as tp
 
