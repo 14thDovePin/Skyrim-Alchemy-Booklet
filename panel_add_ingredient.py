da4e8f01c23aa4ccc7702c946e045e9a70335715
14thDovePin <therlaf0409@gmail.com>
2022-05-25 04:38:26 +0800

    Added dropdown boxes for ingredient effects to "Add Ingredient" panel.
    ► Added some Spinner uix object at "Add Ingredient" panel for selecting the
    currently existing effects.
    ► Created "return_effects" method at "AddIngredient" of
    "panel_add_ingredient.py" for returning all the list of ingredient effects
    currently existing inside the database.

--- panel_add_ingredient.py
+++ panel_add_ingredient.py
@@ -9,3 +9,13 @@ class AddIngredient(ScrollView):
         """Initiates object to communicate with the database."""
         super().__init__(**kwargs)
         self.api = ParseQuery()
+
+    def return_effects(self):
+        """Returns the all list of ingredient's effects from the database."""
+        effects = []
+        effects_raw = [i[4:8] for i in self.api.data_pool]
+        for item in effects_raw:
+            for effect in item:
+                effects.append(effect)
+        effects = set(sorted(effects))
+        return sorted(list(effects))
